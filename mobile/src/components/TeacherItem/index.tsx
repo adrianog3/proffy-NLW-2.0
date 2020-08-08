d9673c83e13989c45e8332a06edcda9caa57e18f
Adriano Souza Arruda <adriano.souza.arruda@gmail.com>
2020-08-08 09:53:58 -0400

    feat(mobile): connected with back-end: handled favorite and teacher list

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,48 +1,60 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, Linking } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 
 import heartOutlineIcon from "../../assets/images/icons/heart-outline.png";
-import unfavoriteIcon from "../../assets/images/icons/unfavorite.png";
+import disfavorIcon from "../../assets/images/icons/disfavor.png";
 import whatsappIcon from "../../assets/images/icons/whatsapp.png";
 
 import styles from "./styles";
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  biography: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function handleLinkToWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
-        <Image
-          style={styles.avatar}
-          source={{ uri: "https://github.com/adrianog3.png" }}
-        />
+        <Image style={styles.avatar} source={{ uri: teacher.avatar }} />
 
         <View style={styles.profileInfo}>
-          <Text style={styles.name}>Adriano Souza</Text>
-          <Text style={styles.subject}>Física</Text>
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text>
         </View>
       </View>
-      <Text style={styles.biography}>
-        Entusiasta das melhores tecnologias de física avançada.
-        {"\n"}
-        {"\n"}
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através da experiência.
-      </Text>
+      <Text style={styles.biography}>{teacher.biography}</Text>
 
       <View style={styles.footer}>
         <Text style={styles.price}>
           Preço/Hora {"   "}
-          <Text style={styles.priceValue}>20,00</Text>
+          <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
         </Text>
 
         <View style={styles.buttonsContainer}>
           <RectButton style={[styles.favoriteButton, styles.isFavorite]}>
             {/* <Image source={heartOutlineIcon} /> */}
-            <Image source={unfavoriteIcon} />
+            <Image source={disfavorIcon} />
           </RectButton>
 
-          <RectButton style={styles.contactButton}>
+          <RectButton
+            style={styles.contactButton}
+            onPress={handleLinkToWhatsapp}
+          >
             <Image source={whatsappIcon} />
             <Text style={styles.contactButtonText}>Entrar em contato</Text>
           </RectButton>
@@ -50,6 +62,6 @@ function TeacherItem() {
       </View>
     </View>
   );
-}
+};
 
 export default TeacherItem;
